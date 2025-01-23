f1b54133127a66e8e2c625e8c47b5b413de80064
Dotori1015 <chaehee9710@gmail.com>
2025-01-23 11:40:14 +0900

    3Day_commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import profile from '../assets/images/profile.png';
+import about from '../assets/images/About.png';
+
+const introText = {
+    title : "port developer",
+    desc : ["talent is","found at the end of the","effort"]
+}
 
 const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
+    return (
+        <section id='intro'>
+            <div className="intro_inner">
+                <h1 className='intro_title'>{introText.title}</h1>
+                <div className="intro_lines" aria-hidden='true'>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                </div>
+                <div className="intro_text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        {/* <img src={profile} alt="프로필 이미지 1"/> */}
+                        <img src={about} alt="프로필 이미지 2"/>
+                    </div>
+                </div>
+                <div className="intro_lines bottom" aria-hidden='true'>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Intro
